67f036183741183e01e53b42842f923f55666758
luclaire03 <clairelu03@gmail.com>
2025-01-18 14:07:36 -0600

    reorganizing

--- iris/src/iris.py
+++ iris/src/iris.py
@@ -13,7 +13,6 @@ import sklearn.metrics as skm
 import seaborn as sns
 import csv
 import random
-import time
 import gseapy as gp
 import scipy
 
@@ -25,6 +24,8 @@ from sklearn.preprocessing import Normalizer
 from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm, decomposition
+
+from iris.utils.generate_bitstrings import generate_bitflip, generate_random
 
 if TYPE_CHECKING:
     from anndata import AnnData
@@ -307,7 +308,7 @@ class IRIS:
     def select_diffusion_components(
             self, 
             trajectory: list[str]
-        ) -> tuple[int, list[int]]:``
+        ) -> tuple[int, list[int]]:
         '''
         For a set of cell type names finds the component on the diffusion maps which best 
         separate the cell types in the correct order and saves it for future analysis.
@@ -771,132 +772,6 @@ class IRIS:
             plt.show()
                     
         return lst_nn_score, lst_resp
-    
-    def generate_bitflip(
-            self, 
-            n: int, 
-            references: list[str]
-        ) -> list:
-        '''
-        Helper function that flips a string of 0's and 1's one-by-one and returns list of 
-        generated sequences. For example, given '101', this function will return 
-        ['001', '111', '100']. Supports multiple reference strings. 
-
-        Args:
-            n: integer number of sequences to generate
-            references: list of strings of reference sequences
-
-        Returns:
-            sequences: list of strings of generated sequences
-        '''
-        sequences = []
-        l = len(references)
-        count = 0
-
-        for i in range(l):
-            combos = []
-            reference = references[i]
-            m = len(reference)
-            for j in range(m):
-                flipped = reference[:j] + str(1-int(reference[j])) + reference[j+1:]
-                combos.append(flipped)
-                count += 1
-                # generated enough from bitflip
-                if count == n:
-                    sequences += combos
-                    return sequences
-                
-            sequences += combos
-        return sequences
-
-    def hamming_distance(
-            self, 
-            string1: str, 
-            string2: str
-        ) -> int:
-        '''
-        Helper function that finds Hamming distance between two strings (sums up 
-        number of characters that differ). Both strings must be same length.
-
-        Args:
-            string1: first string 
-            string2: second string
-
-        Returns:
-            dist: integer Hamming distance
-        '''
-        dist = 0
-        for i in range(len(string1)):
-            if string1[i] != string2[i]:
-                dist += 1
-        return dist
-
-    def generate_random(
-            self, 
-            n: int, 
-            reference: str, 
-            min_distance: int = None, 
-            sampling: bool = False
-        ) -> set:
-        '''
-        Generates n bitstrings that are each min_distance away from all other 
-        generated bitstrings, using reference as the starting point. If min_distance 
-        is not provided, samples increasing distances to find minimum Hamming 
-        distance able to generate n bitstrings. Uses timeout of 30s while sampling.
-
-        Args:
-            n: integer number of bitstrings to be generated
-            reference: string, reference bit sequence (ex. '0110')
-            min_distance: integer minimum distance apart each bitstring should be 
-            sampling: boolean of whether to sample minimum distance or not 
-
-        Returns:
-            combos: set of generated bitstrings, including reference 
-        '''
-        length = len(reference)
-
-        if not min_distance:
-            min_distance = 1
-            sampling = True
-
-        valid = False
-        i = 0
-        combos = {reference}
-        num_to_permute = min_distance
-
-        start = time.time()
-        while i < n:
-            indices = random.sample(range(0,length), num_to_permute)
-            perturbed = ''.join([str(1-int(reference[i])) if i in indices else reference[i] for i in range(length)])
-            if perturbed not in combos:
-                for elem in combos:
-                    if self.hamming_distance(elem, perturbed) < min_distance:
-                        valid = False
-                        break
-                    valid = True
-                if valid:
-                    combos.add(perturbed)
-                    i += 1
-            curr = time.time()
-
-            while curr - start > 30:
-                if num_to_permute < length:
-                    num_to_permute += 1
-                    start = time.time()
-                    break
-                if sampling:
-                    # this distance timed out, so use results from previous one
-                    return self.generate_random(n, reference, min_distance - 1, False)
-                else:
-                    # process timed out with a defined minimum distance
-                    return combos
-            
-        if sampling and min_distance < length:
-            # keep looking only if distance away isn't maxed out 
-            min_distance += 1
-            return self.generate_random(n, reference, min_distance, True)
-
-        return combos
 
     def generate_combinations(
             self, 
@@ -922,11 +797,11 @@ class IRIS:
             list of generated bitstrings
         '''
         num_random = round(num * frac_random)
-        random_combos = list(self.generate_random(num_random, reference, distance))
+        random_combos = list(generate_random(num_random, reference, distance))
 
         num_bitflip = num - num_random
         # assuming only one reference sequence to bitflip off of, but could be more
-        bitflip_combos = self.generate_bitflip(num_bitflip, [reference])
+        bitflip_combos = generate_bitflip(num_bitflip, [reference])
 
         return random_combos + bitflip_combos
 
